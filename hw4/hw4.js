72120ceaf0034ee00dc43a9e9c64e7dfd20f67b5
WForU <wisleybrillian2109@gmail.com>
2024-01-12 19:30:22 +0800

    hw

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -1,87 +1,95 @@
-import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
-import * as render from './render.js';
-import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import{Application, Router, send} from "https://deno.land/x/oak/mod.ts";
 
-const db = new DB("blog.db");
-db.query("Create New Table (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, tel TEXT)");
+const peoples = new Map()
+    peoples.set('Lylia',{
+        name: "Lylia",
+        tel: "12121212",
+        account: "Lylia12@gmail.com",
+        password: "123"
+    });
+    peoples.set('Layla',{
+        name: "Layla",
+        tel: "13131313",
+        account: "Layla13@gmail.com",
+        password: "124",
+    });
 
 const router = new Router();
-
 router
-  .get('/', list)
-  .get('/post/new', add)
-  .get('/post/:id', show)
-  .post('/post', create);
+    .get('/',(ctx) =>{
+        ctx.response.redirect("http://127.0.0.1:8000/public/index.html")
+    })
+
+    .get('/people',(ctx) => {
+        ctx.response.body = Array.from(peoples.values());
+    })
+
+    .post('/people/signup',async(ctx) => {
+        const body = ctx.request.body()
+        if (body.type === "form") {
+          const pairs = await body.value
+          console.log('pairs=', pairs)
+          const params = {}
+          for (const [key, value] of pairs) {
+            params[key] = value
+          }
+          console.log('params=', params)
+          let name = params['name']
+          let account = params['account']
+          let password = params['password']
+          console.log(`account=${account} password=${password}`)
+          if (peoples.get(account)) {
+            ctx.response.type = 'text/html'
+            ctx.response.body = `<p>error: ${account} Already Exist！</p><br/><a href="http://127.0.0.1:8000/public/signup.html">Sign Up Page</a>`
+          } 
+          else {
+            peoples.set(name, {name, account, password})
+            ctx.response.type = 'text/html'
+            ctx.response.body = `<p>${name}Sign Up Successfully</p><a href="http://127.0.0.1:8000/public/login.html">Sign In Page</a>`
+          }
+      
+        }
+      })
+
+    .post('/people/login', async(ctx) =>{
+        const body = ctx.request.body()
+        if (body.type === "form") {
+          const pairs = await body.value
+          console.log('pairs=', pairs)
+          const params = {}
+          for (const [key, value] of pairs) {
+            params[key] = value
+          }
+          console.log('params=', params)
+          let name = params['name']
+          let password = params['password']
+          console.log(`name=${name} password=${password}`)
+          if (peoples.get(name) && password == peoples.get(name).password) {
+            ctx.response.type = 'text/html'
+            ctx.response.body = `<p>Successfully Sign In</p>`
+          } 
+          else {
+            ctx.response.type = 'text/html'
+            ctx.response.body = `<p>Failed to Sign In</p><a href="http://127.0.0.1:8000/public/login.html">Login Again</a>`
+          }
+          console.log("key:",peoples.get(name).password)
+        }
+      })
+
+    .get("/public/(.*)", async (ctx) => {
+        let wpath = ctx.params[0]
+        console.log('wpath=', wpath)
+        await send(ctx, wpath, {
+          root: Deno.cwd()+"/public/",
+          index: "index.html",
+        })
+      })
+
+
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.use(async (ctx, next) => {
-  console.log('path=', ctx.request.url.pathname);
-  if (ctx.request.url.pathname.startsWith("/public/")) {
-    console.log('pass:', ctx.request.url.pathname);
-    await send(ctx, ctx.request.url.pathname, {
-      root: Deno.cwd(),
-      index: "index.html",
-    });
-  } else {
-    await next();
-  }
-});
-
-function query(sql, params = []) {
-  const list = [];
-  for (const [id, name, tel] of db.query(sql, params)) {
-    list.push({ id, name, tel });
-  }
-  return list;
-}
-
-async function list(ctx) {
-  try {
-    const posts = query("SELECT id, name, tel FROM posts");
-    console.log('list:posts=', posts);
-    ctx.response.body = await render.list(posts);
-  } catch (error) {
-    ctx.throw(500, 'Internal Server Error');
-  }
-}
-
-async function add(ctx) {
-  ctx.response.body = await render.newPost();
-}
-
-async function show(ctx) {
-  try {
-    const pid = ctx.params.id;
-    const posts = query("SELECT id, name, tel FROM posts WHERE id=?", [pid]);
-    const post = posts[0];
-    console.log('show:post=', post);
-    if (!post) {
-      ctx.throw(404, 'Invalid post id');
-    }
-    ctx.response.body = await render.show(post);
-  } catch (error) {
-    ctx.throw(500, 'Internal Server Error');
-  }
-}
-
-async function create(ctx) {
-  const body = ctx.request.body();
-  if (body.type === "form") {
-    try {
-      const pairs = await body.value;
-      const post = Object.fromEntries(pairs);
-      console.log('create:post=', post);
-      db.query("INSERT INTO posts (name, tel) VALUES (?, ?)", [post.name, post.tel]);
-      ctx.response.redirect('/');
-    } catch (error) {
-      ctx.throw(500, 'Internal Server Error');
-    }
-  }
-}
-
-const port = parseInt(Deno.args[0]) || 8001;
-console.log(`Server run at http://127.0.0.1:${port}`);
-await app.listen({ port });
+console.log('start at : http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
