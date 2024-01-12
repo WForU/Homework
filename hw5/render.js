282641d202dd20852a065d443245ad29386912ed
WForU <wisleybrillian2109@gmail.com>
2024-01-12 19:58:21 +0800

    hw

--- hw5/render.js
+++ hw5/render.js
@@ -1,104 +1,55 @@
-export function layout(title, content) {
-    return `
-    <html>
-    <head>
-      <title>${title}</title>
-      <style>
-        body {
-          padding: 80px;
-          font: 16px Helvetica, Arial;
-        }
-    
-        h1 {
-          font-size: 2em;
-        }
-    
-        h2 {
-          font-size: 1.2em;
-        }
-    
-        #posts {
-          margin: 0;
-          padding: 0;
-        }
-    
-        #posts li {
-          margin: 40px 0;
-          padding: 0;
-          padding-bottom: 20px;
-          border-bottom: 1px solid #eee;
-          list-style: none;
-        }
-    
-        #posts li:last-child {
-          border-bottom: none;
-        }
-    
-        textarea {
-          width: 500px;
-          height: 300px;
-        }
-    
-        input[type=text],
-        textarea {
-          border: 1px solid #eee;
-          border-top-color: #ddd;
-          border-left-color: #ddd;
-          border-radius: 2px;
-          padding: 15px;
-          font-size: .8em;
-        }
-    
-        input[type=text] {
-          width: 500px;
-        }
-      </style>
-    </head>
-    <body>
-      <section id="content">
-        ${content}
-      </section>
-    </body>
-    </html>
-    `
-  }
-  
-  export function list(posts) {
-    let list = []
-    for (let post of posts) {
-      list.push(`
-      <li>
-        <h2>${ post.title }</h2>
-        <p><a href="/post/${post.id}">Read Post</a></p>
-      </li>
-      `)
-    }
-    let content = `
-    <h1>Posts</h1>
-    <p>You have <strong>${posts.length}</strong> posts!</p>
-    <p><a href="/post/new">Create Post</a></p>
-    <ul id="posts">
-      ${list.join('\n')}
-    </ul>
-    `
-    return layout('Posts', content)
-  }
-  
-  export function newPost() {
-    return layout('New Post', `
-    <h1>New Post</h1>
-    <p>Create a new post.</p>
-    <form action="/post" method="post">
-      <p><input type="text" placeholder="Title" name="title"></p>
-      <p><textarea placeholder="Contents" name="body"></textarea></p>
-      <p><input type="submit" value="Create"></p>
-    </form>
+export function layout(name, content) {
+  return `
+  <html>
+  <head>
+    <title>${name}</title>
+    <link rel="stylesheet" href="/public/main.css">
+  </head>
+  <body>
+    <section id="content">
+      ${content}
+    </section>
+  </body>
+  </html>
+  `
+}
+
+export function list(posts) {
+  let list = []
+  for (let post of posts) {
+    list.push(`
+    <li>
+      <h2>${ post.name }</h2>
+      <p><a href="/post/${post.id}">View Phone Number</a></p>
+    </li>
     `)
   }
-  
-  export function show(post) {
-    return layout(post.title, `
-      <h1>${post.title}</h1>
-      <p>${post.body}</p>
-    `)
-  }
+  let content = `
+  <h1>Contact</h1>
+  <p>You Have <strong>${posts.length}</strong> Contacts!</p>
+  <p><a href="/post/new">Add New Contact</a></p>
+  <ul id="posts">
+    ${list.join('\n')}
+  </ul>
+  `
+  return layout('Posts', content)
+}
+
+export function newPost() {
+  return layout('New Post', `
+  <h1>List</h1>
+  <p>Add New Contact</p>
+  <form action="/post" method="post">
+    <p><input type="text" placeholder="name" name="name"></p>
+    <p><input type="text" placeholder="tel" name="tel""></p>
+    <p><input type="submit" value="Create"></p>
+  </form>
+  `)
+}
+
+export function show(post) {
+  return layout(post.name, `
+    <h1>${post.name}</h1>
+    <p>Phone Number:${post.tel}</p>
+  `)
+}
